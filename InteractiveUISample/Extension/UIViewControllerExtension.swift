d2c848d2c81f39048d860cd47974700cdc4d7bf9
Fumiya Sakai <just1factory@gmail.com>
2021-02-13 23:58:51 +0900

    BackButtonの長押し対応を無効にする

--- InteractiveUISample/Extension/UIViewControllerExtension.swift
+++ InteractiveUISample/Extension/UIViewControllerExtension.swift
@@ -14,8 +14,21 @@ extension UIViewController {
 
     //戻るボタンの「戻る」テキストを削除した状態にするメソッド
     func removeBackButtonText() {
-        let backButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        let backButtonItem = BackBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         self.navigationController!.navigationBar.tintColor = UIColor.white
         self.navigationItem.backBarButtonItem = backButtonItem
+    }
+}
+
+class BackBarButtonItem: UIBarButtonItem {
+    @available(iOS 14.0, *)
+    override var menu: UIMenu? {
+        set {
+            //MEMO: 長押しメニューを消去する
+            //Do Nothing.
+        }
+        get {
+            return super.menu
+        }
     }
 }
